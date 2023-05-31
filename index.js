d10769f55c636b540f259f3cf42845ad100af27f
leilafaez <leila.faez@gmail.com>
2023-05-31 11:56:17 +0100

    fix data and initialize endpoints

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const fs = require("fs");
+const path= require("path");
+const pathToFile= path.resolve("./data.json");
 
+const getResources = ()=>JSON.parse(fs.readFileSync(pathToFile));
 
 app.get("/", (req,res)=>{
     res.send("hello world!")
+})
+
+app.get("/api/resources",(req,res)=>{
+    const resources = getResources();
+    res.send(resources);
 })
 
 app.listen(PORT, ()=>{

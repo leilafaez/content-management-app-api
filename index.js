a36b67e521d3a8961aa4953e331b714f4a46abc4
leilafaez <leila.faez@gmail.com>
2023-06-01 16:49:33 +0100

    add cors control

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const cors = require("cors");
 const fs = require("fs");
 const path= require("path");
 const pathToFile= path.resolve("./data.json");
+
+const corsOptions = {
+    origin :"http://localhost:3000",
+    optionSuccessStatus :200
+}
+
+app.use(cors(corsOptions));
 
 const getResources = ()=>JSON.parse(fs.readFileSync(pathToFile));
 

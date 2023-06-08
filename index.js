91f7877f3ff7cfdfeddb0f8eff1c9087d027b4b5
leilafaez <leila.faez@gmail.com>
2023-06-08 12:11:02 +0100

    control one resource activation on server

--- index.js
+++ index.js
@@ -29,7 +29,19 @@ app.patch("/api/resources/:id", (req, res) => {
   const resources = getResources();
   const { id } = req.params;
   const index = resources.findIndex((resource) => resource.id === id);
+  const activeResource = resources.find(resource => resource.status==="active");
+
   resources[index] =req.body;
+
+  //active resouce related functionality
+  if(req.body.status==="active"){
+    if(activeResource){
+        return res.status(422).send("There is active resource already!")
+    }
+    resources[index].status="active";
+    resources[index].activationTime = new Date();
+  }  
+
   fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
     if (err) {
       return res.status(422).send("cannot edit data in the file!");

77789628f8ce8f9d323af4a641a91053cf1623ea
leilafaez <leila.faez@gmail.com>
2023-06-11 16:42:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT =process.env.PROT || 3001;
+const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const fs = require("fs");
 const path= require("path");

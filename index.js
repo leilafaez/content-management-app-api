a1efaad4e66f7642ea0ea9f3aa64932d83c9ce84
leilafaez <leila.faez@gmail.com>
2023-08-13 23:50:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,180 @@
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const cors = require("cors");
+// const fs = require("fs");
+// const path= require("path");
+// const pathToFile= path.resolve("./data.json");
+
+// const corsOptions = {
+//     origin :"http://localhost:3000",
+//     optionSuccessStatus :200
+// }
+
+// app.use(cors(corsOptions));
+
+// const getResources = ()=>JSON.parse(fs.readFileSync(pathToFile));
+// app.use(express.json())
+
+// app.get("/", (req,res)=>{
+//     res.send("hello world!")
+// })
+// app.get("/api/resources/:id",(req,res)=>{
+//     const resources = getResources();
+//     const {id} = req.params;
+//     const resource = resources.find(resource=>resource.id===id);
+//     res.send(resource);
+// })
+// app.patch("/api/resources/:id", (req, res) => {
+//   const resources = getResources();
+//   const { id } = req.params;
+//   const index = resources.findIndex((resource) => resource.id === id);
+//   const activeResource = resources.find(resource => resource.status==="active");
+
+//   if(resources[index].status === "complete"){
+//     return res.status(422).send("Cannot update because resource has been completed!")
+//   }
+
+//   resources[index] =req.body;
+
+//   //active resouce related functionality
+//   if(req.body.status==="active"){
+//     if(activeResource){
+//         return res.status(422).send("There is active resource already!")
+//     }
+//     resources[index].status="active";
+//     resources[index].activationTime = new Date();
+//   }  
+
+//   fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
+//     if (err) {
+//       return res.status(422).send("cannot edit data in the file!");
+//     }
+//     return res.send("Data has been updated!");
+//   })
+// })
+
+// app.get("/api/activeResource",(req,res)=>{
+//     const resources = getResources();
+//     const activeResource = resources.find(resource=>resource.status==="active");
+//     res.send(activeResource);
+// })
+
+// app.get("/api/resources",(req,res)=>{
+//     const resources = getResources();
+//     res.send(resources);
+// })
+// app.post("/api/resources",(req,res)=>{
+//     const resources = getResources();
+//     const resource = req.body;
+
+//     resource.createdAt = new Date();
+//     resource.status = "inactive";
+//     resource.id = Date.now().toString();
+//     resources.unshift(resource);
+//     fs.writeFile(pathToFile,JSON.stringify(resources,null,2),(err)=>{
+//         if(err){
+//             return res.status(422).send("cannot store data in the file!")
+//         }
+//         return res.send("Data has been saved!")
+//     })
+   
+// })
+// app.listen(PORT, ()=>{
+//     console.log("server is listening on port:" + PORT);
+// })
+
+
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
-const cors = require("cors");
+const PORT = 3001;
+
 const fs = require("fs");
-const path= require("path");
-const pathToFile= path.resolve("./data.json");
+const path = require("path");
+const pathToFile = path.resolve("./data.json");
 
-const corsOptions = {
-    origin :"http://localhost:3000",
-    optionSuccessStatus :200
-}
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 
-app.use(cors(corsOptions));
+app.use(express.json());
 
-const getResources = ()=>JSON.parse(fs.readFileSync(pathToFile));
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get("/", (req,res)=>{
-    res.send("hello world!")
-})
-app.get("/api/resources/:id",(req,res)=>{
-    const resources = getResources();
-    const {id} = req.params;
-    const resource = resources.find(resource=>resource.id===id);
-    res.send(resource);
-})
+app.get("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const resource = resources.find((resource) => resource.id === id);
+  res.send(resource);
+});
+
 app.patch("/api/resources/:id", (req, res) => {
   const resources = getResources();
   const { id } = req.params;
   const index = resources.findIndex((resource) => resource.id === id);
-  const activeResource = resources.find(resource => resource.status==="active");
+  const activeResource = resources.find(
+    (resource) => resource.status === "active"
+  );
 
-  if(resources[index].status === "complete"){
-    return res.status(422).send("Cannot update because resource has been completed!")
+  if (resources[index].status === "complete") {
+    return res
+      .status(422)
+      .send("Cannot update because resource has been completed!");
   }
 
-  resources[index] =req.body;
+  resources[index] = req.body;
 
-  //active resouce related functionality
-  if(req.body.status==="active"){
-    if(activeResource){
-        return res.status(422).send("There is active resource already!")
+  // active resource related functionality
+  if (req.body.status === "active") {
+    if (activeResource) {
+      return res.status(422).send("There is active resource already!");
     }
-    resources[index].status="active";
+
+    resources[index].status = "active";
     resources[index].activationTime = new Date();
-  }  
+  }
+  // active resource related functionality
 
-  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
-    if (err) {
-      return res.status(422).send("cannot edit data in the file!");
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
     }
+
     return res.send("Data has been updated!");
-  })
-})
+  });
+});
 
-app.get("/api/activeResource",(req,res)=>{
-    const resources = getResources();
-    const activeResource = resources.find(resource=>resource.status==="active");
-    res.send(activeResource);
-})
+app.get("/api/activeresource", (req, res) => {
+  const resources = getResources();
+  const activeResource = resources.find(
+    (resource) => resource.status === "active"
+  );
+  res.send(activeResource);
+});
 
-app.get("/api/resources",(req,res)=>{
-    const resources = getResources();
-    res.send(resources);
-})
-app.post("/api/resources",(req,res)=>{
-    const resources = getResources();
-    const resource = req.body;
+app.get("/api/resources", (req, res) => {
+  const resources = getResources();
+  res.send(resources);
+});
 
-    resource.createdAt = new Date();
-    resource.status = "inactive";
-    resource.id = Date.now().toString();
-    resources.unshift(resource);
-    fs.writeFile(pathToFile,JSON.stringify(resources,null,2),(err)=>{
-        if(err){
-            return res.status(422).send("cannot store data in the file!")
-        }
-        return res.send("Data has been saved!")
-    })
-   
-})
-app.listen(PORT, ()=>{
-    console.log("server is listening on port:" + PORT);
-})
+app.post("/api/resources", (req, res) => {
+  const resources = getResources();
+  const resource = req.body;
+
+  resource.createdAt = new Date();
+  resource.status = "inactive";
+  resource.id = Date.now().toString();
+  resources.unshift(resource);
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+
+    return res.send("Data has been saved!");
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port:" + PORT);
+});
